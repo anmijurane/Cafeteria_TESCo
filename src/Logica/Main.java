19d3eb9b5c46582e35c9a94357b457da946b9e58
Andres Jurado <anmijurane@gmail.com>
2020-05-26 22:26:18 -0500

    Iniciamos proyecto

---
+++ src/Logica/Main.java
@@ -0,0 +1,17 @@
+package Logica;
+
+/**
+ *
+ * @author anmijurane <miguel.andres_sic@tesco.edu.mx>
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
