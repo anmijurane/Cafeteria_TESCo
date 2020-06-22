260de7cc01c2c49f0d4583ff2c3695d25af037e1
Andres Jurado <anmijurane@gmail.com>
2020-06-22 15:04:25 -0500

    Se crearon los getters&Setters de la clase proveedor

--- src/poo/proveedor.java
+++ src/poo/proveedor.java
@@ -5,5 +5,35 @@ package poo;
  * @author anmijurane <miguel.andres_sic@tesco.edu.mx>
  */
 public class proveedor {
+    
+    int idProveedor;
+    String Nombre;
+    String telefono;    
 
+    public int getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(int idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+    
 }
